e8f4aef6ef30f4c25b475717c2d3cd9cc027ffe9
Benoit BIZIMANA <benoitbizimana2001@gmail.com>
2024-01-07 01:05:58 +0200

    add forms

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const navLinks = [
     display: 'Home'
   },
   {
-    path: '/docotrs',
+    path: '/doctors',
     display: 'Find a Doctor'
   },
   {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,11 +25,11 @@ const Home = () => {
 
     <section className='hero__section pt-[60px] 2xl:h-[800px]'>
       <div className="container">
-        <div className='flex flex-col lg:flex-row gap-[90] items-center justify-between'>
+        <div className='flex flex-col lg:flex-row gap-[90px] items-center justify-between'>
           {/* ========hero content============== */}
           <div>
             <div className='lg:w-[470px]'>
-              <h1 className='text-[18px] leading-[46px] text-headingColor font-[800] md:text-[60px] 
+              <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] 
                 md:leading-[70px]'>We help patients live a healthy, longer life</h1>
               <p className='text__para'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Amet alias corrupti officia repellat ullam ad provident fuga veniam
@@ -40,9 +40,9 @@ const Home = () => {
             <div className='mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]'>
 
               <div>
-                <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px font-[700] text-headingColor'>30+
+                <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>30+
                 </h2>
-                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[14px]'></span>
                 <p className='text__para'>Years of Experience</p>
               </div>
               <div>
@@ -111,7 +111,7 @@ const Home = () => {
                 
                 
                 >
-                 <BsArrowRight className='group-hover:text-white w6 h-5'/>
+                 <BsArrowRight className='group-hover:text-white w-6 h-5'/>
                 </Link>
             </div>
             </div>
